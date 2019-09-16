977436e20a1247b75e2b279b048abaa1ec611add
theheroesproject <55354250+theheroesproject@users.noreply.github.com>
2019-09-16 16:41:04 +0300

    Update playaudio.js

--- playaudio.js
+++ playaudio.js
@@ -1,8 +1,10 @@
 
+function playmario()
 var myEle = document.getElementById("millenial");
+var clickedbutton = document.getElementById("btn1")
 var song = new Audio;
 song.src = 'Super_Mario.mp3';
-if(myEle != null) { 
+if(myEle != null) 
 	song.load();
-    window.onload=song.play(); 
+    clickedbutton.onclick()=song.play(); 
 }
